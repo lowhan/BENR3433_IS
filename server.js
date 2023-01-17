528da696cc857837893dd8aecab92dd980f9453e
lowhan <b022010033@student.utem.edu.my>
2023-01-18 01:06:12 +0800

    bug fixes

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient; // MongoDB database
 const Admin = require("./admin");                   // Import admin class
 const jwt = require('jsonwebtoken');                // JWT token		
-const express = require('express');
-const fetch = require('node-fetch');
-const { stringify } = require('querystring');
-const formidable = require('formidable');
-var fs = require('fs');
-const app = express();
+const express = require('express');                 // Express server
+const app = express();  
+const fetch = require('node-fetch');                
+const { stringify } = require('querystring');       
 
 // Connection to Database
 MongoClient.connect( 
-	"mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false", 	// main DB for server
+	//"mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false", 	// main DB for server
   //"mongodb://user:qwerty123@10.131.13.195:27017/?authSource=admin&readPreference=primary&directConnection=true&tls=true&tlsCAFile=C%3A%5CProgram+Files%5CMongoDB%5CServer%5C6.0%5Cbin%5Cmongodb3.pem&tlsAllowInvalidCertificates=true",
 	//"mongodb://192.168.1.17",
+  "mongodb://user:qwerty123@192.168.1.17:27017/?tls=true&tlsAllowInvalidHostnames=true&authMechanism=DEFAULT&authSource=admin&tlsCAFile=C%3A%5CProgram+Files%5CMongoDB%5CServer%5C6.0%5Cbin%5Cmongodb.pem",
   { useNewUrlParser: true },
 ).catch(err => {
 	console.error(err.stack)
@@ -22,13 +21,29 @@ MongoClient.connect(
 	Admin.injectDB(client);
 })
 
-app.use(express.static(__dirname + '/'))
+const rateLimit = require('express-rate-limit')     // Limit the rate of sending request 
+// rate limiter to allow user send request within specific number of requests.
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000,     // 10 minutes
+	max: 3,                       // Limit each IP to 3 requests per `window`
+  message: {
+    status: 429,
+    success: false,
+    msg: 'You are doing that too much. Please try again in 10 minutes.'
+   },
+	standardHeaders: true,        // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false,         // Disable the `X-RateLimit-*` headers
+})
+
+app.use(express.static(__dirname + '/'));
 app.use(express.json());
 
+// HTML page is first sent to user
 app.get('/', (_, res) => res.sendFile(__dirname + '/index.html'));
 
-app.post('/login', async (req, res) => {
-  if (!req.body.captcha)      // if no captcha received
+// Login function is made to let user to authenticate and authorise as a role using JWT token
+app.post('/login',limiter, async (req, res) => {
+  if (!req.body.captcha || req.body.captcha == undefined)      // if no captcha received
   {
     return res.json({ success: false, msg: 'Please select captcha' });
   }
@@ -46,16 +61,16 @@ app.post('/login', async (req, res) => {
   
   // Make a request to verifyURL
   const body = await fetch(verifyURL).then(res => res.json());
-
-  // If not successful
+  console.log(body)
+  // If unsuccessful
   if (body.success !== undefined && !body.success)
   {
       return res.json({ success: false, msg: 'Failed captcha verification' });
   }
+  // If successful  
   else 
   {
-    // If successful
-    console.log(body)
+    // Authentication 
     const admin = await Admin.loginadmin({"login_username":req.body.name ,"login_password": req.body.password });
     if (admin == "invalid username"|| admin =='invalid password')
 	  {
@@ -63,14 +78,15 @@ app.post('/login', async (req, res) => {
 	  }
     else
     {
+      // Generate token for authorization
       return res.status(200).json({
-        token : generateAccessToken({								// generate token for authentication, authorization
+        token : generateAccessToken({								
             '_id': admin._id,
             'login_username' : admin.login_username,
             'login_password' : admin.login_password,     
             'security_name' : admin.security_name,
             'security_phonenumber' : admin.security_phonenumber,	
-            'role' : 'admin'
+            'role' : admin.role
         }),
         msg : 'Login success',
         success: true
@@ -79,20 +95,21 @@ app.post('/login', async (req, res) => {
   }  
 });
 
+// View function is made and assume that the user could use the token to authorise as one of the role (like admin) to perform CRUD operations on specific documents.
 app.post('/view', async (req, res) => {
-  //req.body = token
-  var verifiedtoken;
-
-  console.log(req.body);
+  var verifiedtoken;        
   if(req.body.token == '')
   {
     return res.json({ success: false, msg: 'No token was inserted' });
   }
   else
   {
-    verifiedtoken = AccessTokenVerify(req.body.token)
-    console.log(verifiedtoken);
-    if(verifiedtoken.role == 'admin')
+    // verify the content of the token
+    verifiedtoken = AccessTokenVerify(req.body.token)  
+
+    // If the given token has the authorize as admin
+    // console.log(verifiedtoken);          
+    if(verifiedtoken.role == 'admin')                  
     {
       let admin = await Admin.viewadmin(verifiedtoken);
       if(admin == "There is no such account")
@@ -111,34 +128,15 @@ app.post('/view', async (req, res) => {
   }  
 });
 
-//File upload
-app.get('/upload', (req, res) => {
-  res.sendFile(__dirname + '/fileupload.html')
-})
-
-app.post('/upload', (req, res) => {
-  var form = new formidable.IncomingForm()
-  form.uploadDir = "./upload";
-  form.keepExtensions = true;
-  form.maxFieldsSize = 10 * 1024 * 1024;
-  form.multiples = true;
-  form.parse(req, function(err, fields, file) {
-    let filepath = file.upload.filepath;
-    let newpath = __dirname + '/upload/' + file.upload.originalFilename;
-    fs.rename(filepath, newpath, function() {
-      return res.json({ success: true, msg: "file uploaded" });
-    });
-  });
-});
-
 app.listen(3000, () => console.log('Server started on port 3000'));
 
-//Used to prevent HTTP packets leaking the information between parties
-//generate JWT token
+// JWT token - Used to prevent HTTP packets leaking the information between parties
 function generateAccessToken(payload) {
-	return jwt.sign(payload, "my-super-secret", {expiresIn: '1h'}); // set expire time duration
+  // set expire time duration (5 minutes)
+	return jwt.sign(payload, "my-super-secret", {expiresIn: '5m'}); 
 }
-//verify JWT token's content
+
+// JWT token - verify JWT token's content
 function AccessTokenVerify(payload) {
   try {
     return jwt.verify(payload, "my-super-secret");
@@ -147,4 +145,3 @@ function AccessTokenVerify(payload) {
     return err;
   }
 }
-
